51f197f50e9bce28419c5e33745a774961a53d91
Javlonbek Saydullayev <88250263+Saydullayev017@users.noreply.github.com>
2023-11-23 03:46:16 +0300

    Linear Search

---
+++ Linear_Search/linear_search.js
@@ -0,0 +1,18 @@
+// есть у нас массив случайный разбросынных чисел
+const array = [ 1, 4, 5, 8, 1, 2, 7, 2, 11 ];
+let count = 0;
+// создаем функцию
+function lineSearch (array, elem) { // у функции есть два параметра, массив и элемент
+    count += 1
+    // по скольку поиск является линейным, надо последовательно пробигатся по массиву
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === elem) { // если нашли этот элемент
+            return i; // мы возврошаем index массиве
+        }
+    }
+    return null; // если элемент за вес цыкл не нашли венем пустое значения null
+}
+
+console.log(lineSearch (array, 111)) // null
+console.log(lineSearch (array, 11)) // 8
+console.log("count = ", count) // количество итерации
